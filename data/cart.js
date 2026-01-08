d141a9bdbb45e4378fdd9d64f0cc792075239052
anishkushwaha03 <anishsinghkushwaha03@gmail.com>
2026-01-08 18:59:58 +0530

    can select the number of items to add to the cart now

--- data/cart.js
+++ data/cart.js
@@ -1,23 +1,24 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2 ,
-        deliveryOptionId: '1'
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1 ,
-        deliveryOptionId: '2'
-    }];
+    // cart = [{
+    //     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    //     quantity: 2 ,
+    //     deliveryOptionId: '1'
+    // },
+    // {
+    //     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    //     quantity: 1 ,
+    //     deliveryOptionId: '2'
+        
+    // }];
 }
 
 function saveToStorage() {
     localStorage.setItem('cart' , JSON.stringify(cart));
 }
 
-export function addToCart(productId) {
+export function addToCart(productId, quantity) {
     let matchingItem;
 
     cart.forEach((cartItem) => {
@@ -27,11 +28,11 @@ export function addToCart(productId) {
     });
 
     if (matchingItem) {
-        matchingItem.quantity++;
+        matchingItem.quantity += quantity;
     } else {
         cart.push({
             productId: productId,
-            quantity: 1,
+            quantity: quantity,
             deliveryOptionId: '1'
         });
     }
